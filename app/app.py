e7c799c1745684997d5e3e90b88b48c31337c881
Guille1899 <guillem922@hotmail.com>
2021-09-05 10:14:03 +0200

    Server updated

--- app/app.py
+++ app/app.py
@@ -290,4 +290,4 @@ def update_output(n_clicks, value):
 # Run the server
 if __name__ == "__main__":
     # app.run_server(debug=True, port=80)  # Comment this line when launching from the AWS server
-    app.run_server(debug=False, host='0.0.0.0', port=80) # Uncomment this line when launching from the AWS server
+    app.run_server(debug=False, host='0.0.0.0', port=8082) # Uncomment this line when launching from the AWS server
